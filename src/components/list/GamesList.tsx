0d6a80932eea88ace396da9d4767449df7eb6d1d
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-05-06 15:30:54 +0200

    prepared fetch function

--- src/components/list/GamesList.tsx
+++ src/components/list/GamesList.tsx
@@ -1,8 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import GameItem from "./GameItem";
 
 export default function GamesList() {
   const [gamesList, setGamesList] = useState<string[]>(["game1", "game2"]);
+  useEffect(() => {
+    async function getData(){
+      const fetch = await window.fetch(
+        
+        "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15"
+      );
+      const data = fetch.json();
+      console.log(data);
+    }
+  }, []);
 
   return (
     <>
