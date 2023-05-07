504af1bf40925ce4c2799b44afa004bf03e6a24e
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-05-07 15:38:34 +0200

    passed data to GameItem component

--- src/components/list/GameItem.tsx
+++ src/components/list/GameItem.tsx
@@ -1,10 +1,32 @@
 import styled from "styled-components";
 import { GameData } from "./GamesList";
+import Button from "../UI/Button";
 
-const Game = styled.p`
+const Game = styled.div`
   color: red;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-around;
 `;
 
 export default function GameItem(props: GameData) {
-  return <Game>{props.title}</Game>;
+  const metacriticUrl = `https://www.metacritic.com${props.metacriticLink}`;
+  return (
+    <Game>
+      <div className="meta-wrapper">
+        <p>{props.metacriticScore}</p>
+        <a href={metacriticUrl} target="_blank" rel="noreferrer">
+          MORE
+        </a>
+      </div>
+      <div className="prices-wrapper">
+        <p>{props.normalPrice}</p>
+        <p>{props.salePrice}</p>
+      </div>
+      <img src={props.thumb} alt={props.title} />
+      <span>{props.title}</span>
+      <Button text="Go!" />
+    </Game>
+  );
 }
--- src/components/list/GamesList.tsx
+++ src/components/list/GamesList.tsx
@@ -4,6 +4,10 @@ import GameItem from "./GameItem";
 export type GameData = {
   title: string;
   thumb: string;
+  normalPrice: string;
+  salePrice: string;
+  metacriticScore: string;
+  metacriticLink: string;
 };
 
 export default function GamesList() {
@@ -23,7 +27,15 @@ export default function GamesList() {
   return (
     <>
       {gamesList.map((game) => (
-        <GameItem key={game.title} title={game.title} thumb={game.thumb}/>
+        <GameItem
+          key={game.title}
+          title={game.title}
+          thumb={game.thumb}
+          normalPrice={game.normalPrice}
+          salePrice={game.salePrice}
+          metacriticScore={game.metacriticScore}
+          metacriticLink={game.metacriticLink}
+        />
       ))}
     </>
   );
