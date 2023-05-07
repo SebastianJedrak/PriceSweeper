a8023c9d05a2e9173f3ef637de3efc0957c6f3c3
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-05-07 19:25:34 +0200

    deleted GameData type from GamesList component

--- src/components/list/GamesList.tsx
+++ src/components/list/GamesList.tsx
@@ -2,15 +2,6 @@ import { useContext } from "react";
 import GameItem from "./GameItem";
 import { GamesListContext } from "../../ctx/GamesListProvider";
 
-export type GameData = {
-  title: string;
-  thumb: string;
-  normalPrice: string;
-  salePrice: string;
-  metacriticScore: string;
-  metacriticLink: string;
-  storeID: string;
-};
 
 export default function GamesList() {
   const gamesList = useContext(GamesListContext);
