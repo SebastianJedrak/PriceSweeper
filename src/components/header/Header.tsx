fec0c4a6b3c2f269bff8669ada5fe81a3dd5f138
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-05-07 12:25:06 +0200

    deleted nav

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,5 @@
 import BackgroundImg from "./BackgroundImg";
 import Logo from "./Logo";
-import Nav from "./Nav";
 import Search from "./Search";
 import StoresList from "./StoresList";
 
@@ -8,8 +7,6 @@ export default function Header() {
   return (
     <>
       <BackgroundImg>
-        {" "}
-        <Nav />
         <Logo />
         <Search />
         <StoresList/>
