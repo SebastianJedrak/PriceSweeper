afde78c2e7a9a9057296c9a7ddc38027145c8bfb
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-05-07 12:08:55 +0200

    added isOnSale checkbox and prepared StoresList component

--- src/components/header/Search.tsx
+++ src/components/header/Search.tsx
@@ -2,9 +2,10 @@ import styled from "styled-components";
 import Button from "../UI/Button";
 
 const SearchForm = styled.form`
-    display: flex;
-    justify-content: center;
-`
+  display: flex;
+  justify-content: center;
+  color: white;
+`;
 
 export default function Search() {
   return (
@@ -12,6 +13,8 @@ export default function Search() {
       <SearchForm action="">
         <input type="text" placeholder="Search" />
         <Button />
+        <input type="checkbox" id="isOnSale" checked/>
+        <label htmlFor="isOnSale">On Sale</label>
       </SearchForm>
     </>
   );
