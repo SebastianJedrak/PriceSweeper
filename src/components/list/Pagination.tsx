cde9e5aaaa945f98cf80d14838366be3435df997
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-05-08 09:52:09 +0200

    added items per page select

--- src/components/list/Pagination.tsx
+++ src/components/list/Pagination.tsx
@@ -5,17 +5,40 @@ type Props = {
 };
 
 const PaginationWrapper = styled.div`
-text-align:center;
+  display:grid;
+  grid-template-columns: 1/1/1;
+  
+
+  .pages{
+    grid-column: 2/3;
+    text-align:center;
+  }
+
+  .dropdown{
+    grid-column: 3/4;
+text-align: end;
+  }
 `;
 
 export default function Pagination(props: Props) {
   return (
     <PaginationWrapper>
-      <span>&laquo;</span>
-      {[...Array(props.pages)].map((_, i) => {
-        return <span key={i+1}>{i + 1}</span>;
-      })}
-      <span>&raquo;</span>
+      <div className="pages">
+        {" "}
+        <span>&laquo;</span>
+        {[...Array(props.pages)].map((_, i) => {
+          return <span key={i + 1}>{i + 1}</span>;
+        })}
+        <span>&raquo;</span>
+      </div>
+      <div className="dropdown">
+        <label htmlFor="select">Items per page</label>
+        <select id="select">
+          <option>10</option>
+          <option>15</option>
+          <option>20</option>
+        </select>
+      </div>
     </PaginationWrapper>
   );
 }
