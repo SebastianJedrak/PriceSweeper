144554006ee4873d5c6054b5fc4bef0b1fb02e4d
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-05-08 19:03:35 +0200

    fixed non unique key in GamesList bug

--- src/components/list/GamesList.tsx
+++ src/components/list/GamesList.tsx
@@ -22,7 +22,6 @@ export default function GamesList() {
     page * ITEM_PER_PAGE - ITEM_PER_PAGE,
     page * ITEM_PER_PAGE
   );
-
   const numberOfPages = Math.trunc(gamesList.length / 10);
 
   const setPageHandler = (page: number) => {
@@ -38,7 +37,7 @@ export default function GamesList() {
         {" "}
         {gamesPage.map((game) => (
           <GameItem
-            key={game.gameID}
+            key={`${game.gameID}${game.storeID}`}
             title={game.title}
             thumb={game.thumb}
             normalPrice={game.normalPrice}
