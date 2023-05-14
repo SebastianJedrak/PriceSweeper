96d84417ee4b70af0002ce6c3649dc2a5635f5e3
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-05-14 11:44:47 +0200

    deleted BackgroundImage component and transfered img to Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,30 @@
-import BackgroundImg from "./BackgroundImg";
+import styled from "styled-components";
 import Logo from "./Logo";
 import Search from "./Search";
 import StoresList from "./StoresList";
+import backgroundImg from "../../img/main-header-img.png";
+
+const StyledHeader = styled.header`
+  background: linear-gradient(
+      to top,
+      rgba(255, 255, 255, 1) 0%,
+      rgba(255, 255, 255, 0.0) 10%
+    ),
+    url(${backgroundImg});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  padding: 50px 0 ;
+`;
 
 export default function Header() {
   return (
-    <header>
-      <BackgroundImg>
-        <Logo />
-        <Search />
-        <StoresList/>
-      </BackgroundImg>
-    </header>
+    <StyledHeader>
+      {/* <BackgroundImg> */}
+      <Logo />
+      <Search />
+      <StoresList />
+      {/* </BackgroundImg> */}
+    </StyledHeader>
   );
 }
