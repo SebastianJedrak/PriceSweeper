c108a84d7b14364beb10191eacd4c2c516086acc
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-05-16 10:58:12 +0200

    created color palette

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,14 +1,28 @@
 export const theme = {
   primaryColor: {
-    primary300: "",
-    primary400: "",
-    primary500: "",
+    primary200: "#d4efab",
+    primary300: "#bfe881",
+    primary400: "#a9e057",
+    primary500: "#94d82d",
+    primary600: "#85c229",
   },
   secondaryColor: {
-    secondary300: "",
-    secondary400: "",
-    secondary500: "",
+    secondary200: "#fff0d0",
+    secondary300: "#ffe7b1",
+    secondary400: "#ffdd92",
+    secondary500: "#ffd373",
+    secondary600: "#ffce63",
+
+  },
+  weight: {
+    weightSlim: "200",
+    weightNormal: "400",
+    weightBold: "600"
   },
   black: "#000000",
   white: "#FFFFFF",
+  fontPrimaryDark: "#0f1604",
+  fontPrimaryLight: "#f4fbea",
+  fontSecondaryDark: "#19150a",
+  fontSecondaryLight: "#fffaef",
 };
