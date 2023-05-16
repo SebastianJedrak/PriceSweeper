4897d11bf61330de886df08d7506dd54ee4a19e4
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-05-16 10:13:20 +0200

    created object in theme

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,1 +1,14 @@
-export const theme = {}
+export const theme = {
+  primaryColor: {
+    300: "",
+    400: "",
+    500: "",
+  },
+  secondaryColor: {
+    300: "",
+    400: "",
+    500: "",
+  },
+  black: "black",
+  white: "white"
+};
