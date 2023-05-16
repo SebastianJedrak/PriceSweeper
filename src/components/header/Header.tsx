d9f9bfb0db57cd6433384e049354c6f24db12b68
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-05-16 14:27:14 +0200

    changed header img background color

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,10 +8,10 @@ const StyledHeader = styled.header`
   background: linear-gradient(
       to top,
       #fffaef 0%,
-      #1e2b09bb 20%
+      #19150abb 20%
     ),
     url(${backgroundImg});
-  background-size: 140%;
+  background-size: cover;
   background-repeat: no-repeat;
   background-position: top;
   padding: 50px 0 ;
