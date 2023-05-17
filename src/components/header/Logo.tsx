971492417cd7f91d1abe79853fe41477e38706e8
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-05-17 16:44:05 +0200

    prepared description paragraph in Logo

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,19 +1,35 @@
 import styled from "styled-components";
 
-const LogoText = styled.h1`
+const StyledH1 = styled.h1`
   color: ${({ theme }) => theme.secondaryColor.secondaryLight};
   text-align: center;
   span {
     color: ${({ theme }) => theme.primaryColor.primary500};
   }
-  font-family: 'Righteous', cursive;
+  font-family: "Righteous", cursive;
   letter-spacing: 2.5px;
+`;
+
+const StyledParagraph = styled.p`
+  color: ${({ theme }) => theme.secondaryColor.secondaryLight};
+  width: 60%;
+  margin: 0 auto;
+  margin-bottom: 30px;
+  text-align: center;
 `;
 
 export default function Logo() {
   return (
-    <LogoText>
-      Price<span>Sweeper</span>
-    </LogoText>
+    <>
+      <StyledH1>
+        Price<span>Sweeper</span>
+      </StyledH1>
+      <StyledParagraph>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa aliquam
+        vero praesentium ab illo excepturi aliquid, consequatur ex soluta
+        corporis ratione temporibus eveniet amet quia unde quibusdam pariatur
+        ipsum! Exercitationem!
+      </StyledParagraph>
+    </>
   );
 }
