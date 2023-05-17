d0f389f402d3a8ea0973de6384dee4f5530c9241
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-05-17 15:20:09 +0200

    added content to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,26 @@
 import styled from "styled-components";
 
 const FooterWrapper = styled.footer`
-  background-color: ${({theme}) => theme.primaryColor.primaryDark};
+  background-color: ${({ theme }) => theme.primaryColor.primaryDark};
   margin-top: 30px;
   padding: 20px;
-  color: ${({theme}) => theme.secondaryColor.secondaryLight};
+  color: ${({ theme }) => theme.secondaryColor.secondaryLight};
   text-align: center;
+
+  a {
+    color: ${({ theme }) => theme.secondaryColor.secondaryLight};
+    text-decoration: underline;
+  }
 `;
 
 export default function Footer() {
   return (
     <FooterWrapper>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae distinctio magnam repudiandae illo dicta fuga ratione? Rem exercitationem minima corrupti quibusdam repellat facilis ea minus perferendis, pariatur, aliquam delectus, ut nostrum accusantium quas dignissimos veniam. Quas dolorum amet atque sunt quibusdam, voluptatum magni quisquam et, quo obcaecati iste sint velit! </p>
+      <p>
+        Website made by Sebastian Jędrak for portfolio purposes. If you have any problem you can contact me at jedrak.sebastian@gmail.com </p> <span> All offers are real, any buy contribute </span>
+        <a target="_blank" rel="noreferrer" href="https://www.cheapshark.com">
+          cheapshark.com
+        </a>{" "}
     </FooterWrapper>
   );
 }
